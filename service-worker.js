73c9d6b965477eaa90556d7ef863a5b897daf90b
vladkhitrik <vladkhitrik@users.noreply.github.com>
2026-07-09 12:35:56 +1000

    Restrict corner home button pages and refresh SW caching

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "glex-v3";
+const CACHE_NAME = "glex-v4";
 const CORE_ASSETS = [
   "./",
   "./index.html",
@@ -57,16 +57,12 @@ self.addEventListener("fetch", (event) => {
   }
 
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      const networkFetch = fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
-          return response;
-        })
-        .catch(() => cached);
-
-      return cached || networkFetch;
-    })
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
